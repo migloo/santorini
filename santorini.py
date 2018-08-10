4c8aff63c170ab0ae31e355144e4ae2de5297b25
Clément Miglietti <migloo@gmail.com>
2018-08-10 19:08:36 +0200

    mid point commit: possibles moves

--- santorini.py
+++ santorini.py
@@ -1,9 +1,14 @@
 import numpy as np
 
-# représentation d'un joueur: une fonction d'évaluation.
-class Player():
+PLAYER0 = 0
+PLAYER1 = 1
+
+class Player:
     def evaluate_position():
         # fonction qui évalue une position
+        # MinMax aglorightm https://en.wikipedia.org/wiki/Minimax
+        # avec une évaluation naive en fonction de la hauteur des
+        # pion du joueur.
         return 0
 
     def play(possible_moves, evaluate):
@@ -14,29 +19,32 @@ class Player():
 
 
 class Santorini:
-    # représentation d'un état du jeu
-    # - matrice 5x5, avec niveau 0 à 4
-    # - position joueur 0, 2x tuple
-    # - position joueur 1, 2x tuple.
-    # - PLAYER_TO_PLAY: un int, 0 ou 1, qui indique à qui le tour.
-    # - EOG: boolean qui indique si le jeu est terminé ou pas.
-    matrix board
-
-    # un mouvement:
-    # - matrice 5x5, avec niveau 0 ou 1
-    # - mouvement joueur 1, 2x tuple
-    # - mouvement joueur 2, 2x tuple
-
-    def Santorini(player1, player2):
+    def __init__(self):
         # constructeur
+        self.moves = [(0,1), (1,1), (1,0), (1,-1), (0,-1), (-1,-1), (-1,0), (-1,1)]
+        self.pawns = [[(2,3),(3,2)],[(3,4),(4,3)]]
+        self.board = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
+        self.EOG = False
+        self.PLAYER_TO_PLAY = PLAYER0
 
-    def list_possible_moves():
-        # fonction qui liste les mouvements possibles
-        return []
+    def move_allowed(position_start, position_end):
+        (i,j) = position_start
+        (a,b) = position_end
+        on_board = (a>-1) and (a<6) and (b>-1) and (b<6)
+        not_occupied = not(x in self.pawns[0]) and not(x in self.pawns[1])
+        level_4 = self.board[a][b] < 4
+        jump_max_1 = self.board[a][b] - self.board[i][j] <= 1
+        return on_board and not_occupied and level_4 and jump_max_1
+
+    def list_possible_moves(self):
+        # return a list of reachable board states.
+        position_start = pawns[self.PLAYER_TO_PLAY][0]
+        pawn_moves = [tuple(map(sum, zip(position_start, x))) for x in self.moves]
 
     def game():
         # fonction qui simule un match entre 2 joueurs.
         while not EOG:
+            pass
             # next player joue.
         return not PLAYER_TO_PLAY
 
@@ -46,3 +54,8 @@ def run():
     player2 = Player()
     s = Santorini(player1, player2)
     winner = s.game()
+
+# faire jouer 1 milion de parties
+# recorder les configurations
+# classer les configurations suivant qu'elles appartiennent à un arbre gagnant ou perdant
+# incorporer l'histoire dans la fonction d'évaluation
